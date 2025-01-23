d3d16022d2b97a90b2ab4fbc8cc27d68354f0ab0
kevh182 <154713523+kevh182@users.noreply.github.com>
2025-01-23 10:04:02 -0500

    Create Create_Folders_From_Filenames.ps1

---
+++ Functions/Create_Folders_From_Filenames.ps1
@@ -0,0 +1,28 @@
+﻿function FoldersFromFiles {
+
+    $ErrorActionPreference = "SilentlyContinue"
+
+    Write-Host "==========================================================" -ForegroundColor DarkYellow
+    Write-Host " Create Folders from Filenames " -ForegroundColor DarkYellow
+    Write-Host "==========================================================" -ForegroundColor DarkYellow
+
+    # Set the path to the directory containing your files
+    $sourcePath = Read-Host "Enter ROM Path"
+
+    # Get a list of files in the directory
+    $fileList = Get-ChildItem -Path $sourcePath -Recurse
+        
+    # Iterate through each file
+    foreach ($file in $fileList) {
+
+        # Create a folder with the file name (without extension)
+        $folderName = [System.IO.Path]::GetFileNameWithoutExtension($file.Name)
+        $folderPath = Join-Path -Path $sourcePath -ChildPath $folderName
+
+        New-Item -ItemType Directory -Path $folderPath -Force
+
+        # Move the file into the corresponding folder
+        Move-Item -Path $file.FullName -Destination $folderPath
+
+      }
+}
